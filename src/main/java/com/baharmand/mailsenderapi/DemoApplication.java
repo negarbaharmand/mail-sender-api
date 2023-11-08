fbe6cf6d9dd993a477c1219c005e3078df75b3aa
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-08 18:07:36 +0100

    Initial commit

---
+++ src/main/java/com/baharmand/mailsenderapi/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.baharmand.mailsenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
